3267255da2af7b71ba6d77fc2c51bf2e441b6d64
akhalizev <ahalizev@gmail.com>
2020-08-18 10:55:37 -0500

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+let city = {
+  name: "Minneapolis",
+  country: "Minnesota",
+};
+
+function Hello({ intro, message }) {
+  return (
+    <div>
+      <h1>{intro}</h1>
+      <p>{message}</p>
+    </div>
+  );
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Hello intro="Welcome to React" message="Let's learn some stuff" />,
+  document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
