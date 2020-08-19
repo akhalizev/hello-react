7757b2530c930055888f73f52d87841a83581513
akhalizev <ahalizev@gmail.com>
2020-08-19 13:34:02 -0500

    Fraction

--- src/index.js
+++ src/index.js
@@ -2,18 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-function Lake({ name }) {
-  return <h1>{name}</h1>;
-}
+const lakeList = [
+  { id: "1", name: "Lake of Isles", traildhead: "Easy" },
+  { id: "2", name: "Bde Maka Ska", traildhead: "Mod" },
+  { id: "3", name: "Harriet", traildhead: "Hard" },
+];
 
-function App() {
+const list = [1, 2, 3, 4, 5];
+
+function App({ items }) {
   return (
-    <div>
-      <Lake name="Lake Tahoe"></Lake>
-      <Lake name="Lake Calhoun"></Lake>
-      <Lake name="Lake Superior"></Lake>
-    </div>
+    <ul>
+      {items.map((item, i) => (
+        <div>
+          <li key={i}>{item} – Item in the list</li>
+          <li>ID of the item {i}</li>
+        </div>
+      ))}
+    </ul>
   );
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App items={list} />, document.getElementById("root"));
